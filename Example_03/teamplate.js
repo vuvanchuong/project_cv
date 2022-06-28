da7eaf5478a9013270635a002814149563bec2d0
Vũ Văn Chưởng <chuongvu20102001@gmail.com>
2022-06-28 20:22:31 +0700

    project

---
+++ Example_03/teamplate.js
@@ -0,0 +1,9 @@
+var myslider = document.getElementById('myslider');
+
+function Open() {
+    if (myslider.style.display === 'none') {
+        myslider.style.display = 'block';
+    } else {
+        myslider.style.display = 'none';
+    }
+}
